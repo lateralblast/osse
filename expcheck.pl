99c6426e0fafcba392318f482da442f7d22abc3a
Richard Spindler <richard@lateralblast.com.au>
2013-08-19 11:27:38 +1000

    Added initial security check support

--- expcheck.pl
+++ expcheck.pl
@@ -6,7 +6,7 @@ use Getopt::Std;
 use File::Basename;
 
 # Name:         expcheck.pl
-# Version:      0.0.7
+# Version:      0.0.9
 # Release:      1
 # License:      Open Source 
 # Group:        System
@@ -31,12 +31,16 @@ use File::Basename;
 #               Added Kerberos and individual explorer file support
 #               0.0.7 Mon 19 Aug 2013 08:57:22 EST
 #               Added check to make sure host isn't duplicated (old explorers)
+#               0.0.8 Mon 19 Aug 2013 10:21:21 EST
+#               Converted search to array to avoid multiple file opens of the same file
+#               0.0.9 Mon 19 Aug 2013 11:26:26 EST
+#               Added initial security check
 
 my $script_name=$0;
 my $script_version=`cat $script_name | grep '^# Version' |awk '{print \$3}'`;
 my $explorer_dir="explorers";
 my %option=();
-my $options="hBHJPRVc:f:m:o:s:";
+my $options="hBHJPRSVc:f:m:o:s:";
 
 if ($#ARGV == -1) {
   print_usage();
@@ -77,6 +81,7 @@ sub print_usage {
   print "-B: Report which machines have BSM enabled\n";
   print "-K: Report which machines have Kerberos enabled\n";
   print "-R: Report which machines have RSA SecurID PAM agent installed\n";
+  print "-S: Run security check against explorers\n";
   print "-H: Generate HTML report\n";
   print "-s: String based search\n";
   print "-f: Explorer file to search\n";
@@ -98,29 +103,65 @@ if ($option{'s'}) {
   search_explorers($option{'f'},$option{'s'},$option{'m'},$option{'c'});
 }
 
+if ($option{'S'}) {
+  security_check($option{'c'});
+  exit;
+}
+
 if ($option{'K'}) {
-  krb_status();
+  krb_status($option{'c'});
   exit;
 }
 
 if ($option{'R'}) {
-  rsa_status();
+  rsa_status($option{'c'});
   exit;
 }
 
 if ($option{'B'}) {
-  bsm_status();
+  bsm_status($option{'c'});
   exit;
 }
 
 if ($option{'J'}) {
-  jass_status();
+  jass_status($option{'c'});
   exit;
 }
 
 if ($option{'P'}) {
-  puppet_status();
+  puppet_status($option{'c'});
   exit;
+}
+
+sub security_check {
+  my $search_client=$_[0];
+  my $search_message="Set";
+  my $search_string;
+  my $search_file;
+  $search_string="^DISABLETIME=3600,^SYSLOG=YES,^SYSLOG_FAILED_LOGINS=0";
+  $search_file="etc/default/login";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^MAXWEEKS=48,^MAXREPEATS=0";
+  $search_file="etc/default/passwd";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^ENABLE_NOBODY_KEYS=YES";
+  $search_file="etc/default/keyserv";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^TCP_STRONG_ISS=2";
+  $search_file="etc/default/inetinit";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^PMCHANGEPERM=-,^CPRCHANGEPERM=-";
+  $search_file="etc/default/power";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^PERMS=-";
+  $search_file="etc/default/sys-suspend";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^LOG_FOR_REMOTE=NO";
+  $search_file="etc/default/syslogd";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
+  $search_string="^BANNER=\"Authorized Use Only\"";
+  $search_file="etc/default/telnetd";
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub create_template {
@@ -147,38 +188,43 @@ sub get_explorer_list {
 }
 
 sub krb_status {
+  my $search_client=$_[0];
   my $search_string="other[[:space:]]*auth[[:space:]]*sufficient[[:space:]]*pam_krb5.so.1";
   my $search_file="etc/pam.conf";
   my $search_message="Installed";
-  search_explorers($search_file,$search_string,$search_message);
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub jass_status {
+  my $search_client=$_[0];
   my $search_string="SUNWjass";
   my $search_file="patch+pkg/pkginfo-l.out";
   my $search_message="Installed";
-  search_explorers($search_file,$search_string,$search_message);
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub puppet_status {
+  my $search_client=$_[0];
   my $search_string="puppet";
   my $search_file="patch+pkg/pkginfo-l.out";
   my $search_message="Installed";
-  search_explorers($search_file,$search_string,$search_message);
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub bsm_status {
+  my $search_client=$_[0];
   my $search_string="audit";
   my $search_file="etc/system";
   my $search_message="Enabled";
-  search_explorers($search_file,$search_string,$search_message);
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub rsa_status {
+  my $search_client=$_[0];
   my $search_string="securid";
   my $search_file="etc/pam.conf";
   my $search_message="Enabled";
-  search_explorers($search_file,$search_string,$search_message);
+  search_explorers($search_file,$search_string,$search_message,$search_client);
 }
 
 sub search_explorers {
@@ -187,6 +233,7 @@ sub search_explorers {
   my $search_message=$_[2];
   my $search_client=$_[3];
   my @explorer_list=get_explorer_list($search_client);
+  my @search_string;
   my @host_list;
   my $hostname;
   my $pkg_file;
@@ -200,6 +247,13 @@ sub search_explorers {
   my $command;
   my $template;
   my $output_file;
+  if ($search_string=~/\,/) {
+    @search_string=split(",",$search_string);
+  }
+  else {
+    @search_string[0]=$search_string; 
+  }
+  $search_string="";
   $search_file=~s/^\///g;
   if ($option{'H'}) {
     $template=create_template();
@@ -219,31 +273,35 @@ sub search_explorers {
     $command="gtar -xpzf $explorer $filename -O";
     @pkg_info=`$command`;
     if (!grep /$hostname/,@host_list) {
-      if (grep /$search_string/,@pkg_info) {
-        if ($option{'H'}) {
-          my %row=(hostname=>"$hostname", value=>"<font color=\"green\">$search_string $search_message</font>");
-          push(@loop,\%row);
-        }
-        else {
-          if ($option{'o'}) {
-            print FILE "$hostname: $search_string $search_message\n";
-          }
-           else {
-            print "$hostname: $search_string $search_message\n";
-          }
-        }
-      }
-      else {
-        if ($option{'H'}) {
-          my %row=(hostname=>"$hostname", value=>"<font color=\"red\">$search_string Not $search_message</font>");
-          push(@loop,\%row);
-        }
-        else {
-          if ($option{'o'}) {
-            print FILE "$hostname: $search_string Not $search_message\n";
+      foreach $search_string (@search_string) {
+        if (grep /$search_string/,@pkg_info) {
+          $search_string=~s/^\^//g;
+          if ($option{'H'}) {
+            my %row=(hostname=>"$hostname", value=>"<font color=\"green\">$search_string $search_message</font>");
+            push(@loop,\%row);
           }
           else {
-            print "$hostname: $search_string Not $search_message\n";
+            if ($option{'o'}) {
+              print FILE "$hostname: $search_string $search_message\n";
+            }
+            else {
+              print "$hostname: $search_string $search_message\n";
+            }
+          }
+        }
+        else {
+          $search_string=~s/^\^//g;
+          if ($option{'H'}) {
+            my %row=(hostname=>"$hostname", value=>"<font color=\"red\">$search_string Not $search_message</font>");
+            push(@loop,\%row);
+          }
+          else {
+            if ($option{'o'}) {
+              print FILE "$hostname: $search_string Not $search_message\n";
+            }
+            else {
+              print "$hostname: $search_string Not $search_message\n";
+            }
           }
         }
       }
